21a3999f33d613d478497c9f60dbef07402386e1
Tanya <krajnikova2004@gmail.com>
2024-08-14 21:06:43 +0300

    Create class to manage the flow between ContactReadModel and ContactReadView, retrieving and formatting contact data for display.

---
+++ src/app/controllers/ContactReadController.java
@@ -0,0 +1,35 @@
+package app.controllers;
+
+import app.models.ContactReadModel;
+import app.views.ContactReadView;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ContactReadController {
+
+    ContactReadModel model;
+    ContactReadView view;
+
+    public ContactReadController(ContactReadModel model, ContactReadView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public String readContacts() {
+        Map<String, String> map = model.readContacts();
+        AtomicInteger count = new AtomicInteger(0);
+        StringBuilder stringBuilder = new StringBuilder();
+        map.forEach((key, value) ->
+                stringBuilder.append(count.incrementAndGet())
+                        .append(") ")
+                        .append(key)
+                        .append(" ")
+                        .append(value)
+                        .append("\n")
+        );
+        return stringBuilder.toString();
+    }
+
+
+}
