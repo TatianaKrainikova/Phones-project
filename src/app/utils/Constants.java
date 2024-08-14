a5999d43b3a6a7020b519ea425473fa23528d3b7
Tanya <krajnikova2004@gmail.com>
2024-08-14 18:37:37 +0300

    Create class with constant messages

---
+++ src/app/utils/Constants.java
@@ -0,0 +1,9 @@
+package app.utils;
+
+public class Constants {
+    public final static String CLOSE_APP_MSG = "\n>> App has closed.";
+    public static String NAN_MSG = "\"%s\" - not a number!%nTry again: ";
+    public final static String NO_SUCH_OPTION_MSG = "There is no such option! Try again: ";
+    public final static String NAME_EMPTY_MSG = "Empty! Please enter a name: ";
+    public final static String PHONE_EMPTY_MSG = "Empty! Enter phone: ";
+}
