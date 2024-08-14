2857457ad832352a68e5bcdc00f05cbb83f215f8
Tanya <krajnikova2004@gmail.com>
2024-08-14 18:00:55 +0300

    Create an object class-model

---
+++ src/app/database/entities/Contact.java
@@ -0,0 +1,23 @@
+package app.database.entities;
+
+public class Contact {
+
+    String name;
+    String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
